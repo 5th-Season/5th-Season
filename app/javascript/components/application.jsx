b6a34caefec8e5f26e98a5dc5ac787c4bfa2d7be
Lawrence S <lboogie@Lawrences-Air-2.attlocal.net>
2024-03-31 22:10:30 -0500

    Add esbuild

---
+++ app/javascript/components/application.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+
+
+const App = ({ arg }) => {
+ return <div>{`Hello, ${arg}!`}</div>;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Loading React")
+  const rootEl = document.getElementById("root");
+ ReactDOM.render(<App arg="Rails 7 with ESBuild" />, rootEl);
+});
