7dbe7cff8caee8ac7bef51d02ec854a67231de18
Lawrence S <lboogie@Lawrences-Air-2.attlocal.net>
2024-06-06 16:54:34 -0500

    clean up style

--- app/javascript/components/GridItem.jsx
+++ app/javascript/components/GridItem.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const GridItem = () => {
+const GridItem = (props) => {
     return (
         <div className="gridItem">
             <div className="innerGridItem">
@@ -27,10 +27,10 @@ const GridItem = () => {
 
                     </div>
                     <div className="UserInfo-Data">
-                        <div className="UserInfo-Name">Larry S</div>
+                        <div className="UserInfo-Name">{props.name}</div>
                         <div className="UserInfo-Location">
                           <svg xmlns="http://www.w3.org/2000/svg" viewBox="-4808 -20688 14.286 20" className="UserSummaryInfo-locationIcon"><g><path d="M-4800.857-20688a7.143 7.143 0 0 0-7.143 7.143c0 5.714 7.143 12.857 7.143 12.857s7.143-7.143 7.143-12.857a7.142 7.142 0 0 0-7.143-7.143zm0 10a2.857 2.857 0 1 1 2.857-2.859 2.858 2.858 0 0 1-2.857 2.859z"></path></g></svg>
-                          <span>Houston, TX</span>
+                          <span>{props.location}</span>
                         </div>
                         <div className="UserInfo-Available">
                           <button><span>Featured</span></button>
--- app/javascript/components/collaborate.jsx
+++ app/javascript/components/collaborate.jsx
@@ -7,12 +7,42 @@ import GridItem from "./GridItem";
 const Collab = ({ arg }) => {
  return (
     <div className="CollabGrid">
-        <GridItem />
-        <GridItem />
-        <GridItem />
-        <GridItem />
-        <GridItem />
-        <GridItem />
+        <GridItem
+          name={"Larry S"} 
+          location={"Cleveland, OH"}
+        />
+        <GridItem
+          name={"Chanel"}
+          location={"Houston, TX"}
+        />
+        <GridItem
+          name={"Albert"}
+          location={"Dallas, TX"} 
+        />
+        <GridItem
+          name={"Albert"}
+          location={"New Orleans, LA"} 
+        />
+        <GridItem
+          name={"Albert"} 
+          location={"New York, NY"}
+        />
+        <GridItem
+          name={"Albert"} 
+          location={"Buffalo, NY"}
+        />
+         <GridItem
+          name={"Albert"}
+          location={"New Orleans, LA"} 
+        />
+        <GridItem
+          name={"Albert"} 
+          location={"New York, NY"}
+        />
+        <GridItem
+          name={"Albert"} 
+          location={"Buffalo, NY"}
+        />
     </div>
  );
 };
