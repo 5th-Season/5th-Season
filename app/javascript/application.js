89db2098546951bb42687545619253afd03d0259
Lawrence S <lboogie@Lawrences-MacBook-Air-2.local>
2024-08-15 14:16:56 -0500

    remove css files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "tailwindcss/base";
-import "tailwindcss/components";
-import "tailwindcss/utilities";
-import "./stylesheets/index.scss";
+// import "tailwindcss/base";
+// import "tailwindcss/components";
+// import "tailwindcss/utilities";
+// import "./stylesheets/index.scss";
 import "./components";
 //import "controllers"
 // import "./components/application";
