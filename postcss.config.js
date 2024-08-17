3a4b59914840d3ef4fb64d863851f7459d9de69f
Lawrence S. <lboogie@Lawrences-MBP.attlocal.net>
2024-08-17 00:39:45 -0500

    Adjust for Heroku deployment

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 const {sassPlugin} = require('esbuild-sass-plugin');
+var tailwindcss = require("tailwindcss");
 module.exports = {
   plugins: [
+    tailwindcss("./config/tailwind.config.js"),
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
