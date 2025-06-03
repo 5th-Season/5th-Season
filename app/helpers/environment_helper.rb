3d9fb07ce3d9eb98d095a4c092fe28c051ca7c8a
Lawrence S. <lboogie@MacBookPro.attlocal.net>
2025-06-03 17:16:09 -0500

    Fix signup form submission issue on Heroku by making environment helper more robust

--- app/helpers/environment_helper.rb
+++ app/helpers/environment_helper.rb
@@ -2,12 +2,15 @@ module EnvironmentHelper
   def environment_variables
     env_vars = {
       NODE_ENV: Rails.env,
-      MIXPANEL_TOKEN: ENV['MIXPANEL_TOKEN']
+      MIXPANEL_TOKEN: ENV['MIXPANEL_TOKEN'] || ''
     }
 
     # You can add more environment variables here that you want to expose to the front-end
     # Be careful not to expose sensitive information like API secrets
     
     raw "window.__ENV__ = #{env_vars.to_json};"
+  rescue => e
+    Rails.logger.error "Error generating environment variables: #{e.message}"
+    raw "window.__ENV__ = { NODE_ENV: '#{Rails.env}', MIXPANEL_TOKEN: '' };"
   end
 end
